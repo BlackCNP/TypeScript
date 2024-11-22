b23e95ad7f61e412466a1855aad6d38540e9e2ff
BlackCNP <90748408+BlackCNP@users.noreply.github.com>
2024-11-22 19:25:28 +0200

    Add test

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ class ContentValidator {
 }
 const articleValidator = new ContentValidator();
 const productValidator = new ContentValidator();
+// Тест
+const article = {
+    id: '1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    status: 'draft',
+    title: 'Заголовок',
+    content: 'Шось написав.',
+    authorId: 'Koristuvach',
+    tags: ['новини', 'стаття'],
+};
+const articleValidationResult = articleValidator.validate(article);
+if (articleValidationResult.isValid) {
+    console.log('Стаття валідна:', article);
+}
+else {
+    console.error('Помилки валідації:', articleValidationResult.errors);
+}
+// доступ
+const accessControl = {
+    role: 'editor',
+    permissions: {
+        create: true,
+        read: true,
+        update: true,
+        delete: false,
+    },
+    canAccess: (content, action) => {
+        // все, крім видалення
+        return action !== 'delete';
+    }
+};
+// Перевірка доступу
+if (accessControl.canAccess(article, 'read')) {
+    console.log('Користувач може читати статтю.');
+}
+if (accessControl.canAccess(article, 'delete')) {
+    console.log('Користувач може видалити статтю.');
+}
+else {
+    console.log('Користувач не може видалити статтю.');
+}
--- script.ts
+++ script.ts
@@ -88,7 +88,52 @@ interface BaseContent {
     rollback: (version: number) => T | null;
   }
   
+   // Тест
+   const article: Article = {
+    id: '1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    status: 'draft',
+    title: 'Заголовок',
+    content: 'Шось написав.',   
+    authorId: 'Koristuvach',
+    tags: ['новини', 'стаття'],
+  };
   
+ 
+  const articleValidationResult = articleValidator.validate(article);
+  
+  if (articleValidationResult.isValid) {
+    console.log('Стаття валідна:', article);
+  } else {
+    console.error('Помилки валідації:', articleValidationResult.errors);
+  }
+  
+  // доступ
+  const accessControl: AccessControl<Article> = {
+    role: 'editor',
+    permissions: {
+      create: true,
+      read: true,
+      update: true,
+      delete: false,
+    },
+    canAccess: (content, action) => {
+      // все, крім видалення
+      return action !== 'delete'; 
+    }
+  };
+  
+  // Перевірка доступу
+  if (accessControl.canAccess(article, 'read')) {
+    console.log('Користувач може читати статтю.');
+  }
+  
+  if (accessControl.canAccess(article, 'delete')) {
+    console.log('Користувач може видалити статтю.');
+  } else {
+    console.log('Користувач не може видалити статтю.');
+  }
 
   
  
